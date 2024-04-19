57687bd0946deb012f79193afe4add6d7a9c3412
Jahid Hasan <jh3495689@gmail.com>
2024-04-19 17:32:29 +0600

    added all code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { LuEye, LuEyeOff } from "react-icons/lu";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import facebookLogo from "../assets/facebook.png";
 import metaLogo from "../assets/meta.png";
 
 const Home = () => {
   const [errorMessage, setErrorMessage] = useState("");
+  const [show, setShow] = useState(true);
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -45,6 +47,10 @@ const Home = () => {
     }
   };
 
+  const handleShowPass = () => {
+    setShow(!show);
+  };
+
   return (
     <div className="bg-[#F0F2F5] pt-20 grid justify-center text-center">
       <div>
@@ -57,7 +63,7 @@ const Home = () => {
           src={facebookLogo}
           alt="Facebook"
         />
-        <form onSubmit={handleLogin} className="grid gap-5">
+        <form onSubmit={handleLogin} className="grid gap-5 w-3/4 mx-auto">
           <input
             className="block px-4 py-3 rounded-[10px]"
             placeholder="Mobile number or email address"
@@ -65,18 +71,27 @@ const Home = () => {
             type="text"
             required
           />
-          <input
-            className="block px-4 py-3 rounded-[10px]"
-            placeholder="Password"
-            name="password"
-            type="password"
-            required
-          />
+          <div className="relative w-full">
+            <input
+              className="block px-4 py-3 w-full rounded-[10px]"
+              placeholder="Password"
+              name="password"
+              type={`${show ? "password" : "text"}`}
+              required
+            />
+            <div
+              className="absolute top-1/3 right-5"
+              onClick={() => handleShowPass()}
+            >
+              {show ? <LuEyeOff /> : <LuEye />}
+            </div>
+          </div>
           <input
             className="px-4 py-3 bg-[#1877F2] text-white rounded-full cursor-pointer"
             type="submit"
             value="Log in"
           />
+          <p></p>
           {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         </form>
         <p className="text-blue-500 cursor-pointer">Forgotten password?</p>
